6d7e5c7b1cb694091b53b90dab5e2f38a3d3e1d2
Samuel Scozzari <s_scozzari@libero.it>
2022-01-19 00:17:53 +0100

    Right side Completed - Milestone 1 completed

--- js/vue.js
+++ js/vue.js
@@ -46,6 +46,50 @@ new Vue({
           },
         ],
       },
+      {
+        name: "Samuele",
+        avatar: "_3",
+        visible: true,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Hai portato a spasso il cane?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Ricordati di dargli da mangiare",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 16:15:22",
+            text: "Tutto fatto!",
+            status: "received",
+          },
+        ],
+      },
+      {
+        name: "Luisa",
+        avatar: "_6",
+        visible: true,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Hai portato a spasso il cane?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Ricordati di dargli da mangiare",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 16:15:22",
+            text: "Tutto fatto!",
+            status: "received",
+          },
+        ],
+      },
     ],
   },
 });
